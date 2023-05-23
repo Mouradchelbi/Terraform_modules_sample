f46a0c2847f92b4f15770ca8e19fc280b213de7c
Mouradchelbi <72913289+Mouradchelbi@users.noreply.github.com>
2023-05-23 11:57:06 +0200

    Add files via upload

---
+++ ResourceGroup/variables.tf
@@ -0,0 +1,9 @@
+variable "base_name" {
+    type = string
+    description = "base name for resource group and account storage"
+
+}
+variable "location" {
+    type = string
+    description = "the location of deployment"
+}
