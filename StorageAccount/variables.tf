99744798a306c46660f18ba1760776a421ec0d4e
Mouradchelbi <72913289+Mouradchelbi@users.noreply.github.com>
2023-05-23 11:57:41 +0200

    Add files via upload

---
+++ StorageAccount/variables.tf
@@ -0,0 +1,16 @@
+variable "base_name" {
+    type = string
+    description = "the storage account"
+}
+variable "resource_group_name" {
+    type = string
+    description = "Name of the resource"
+
+ 
+}
+
+variable "location" {
+    type = string
+    description = "the location of the deployment"
+  
+}
